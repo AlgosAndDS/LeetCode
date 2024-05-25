d88cb98e723e1688467697160ab6c3e84d03755b
Andre <mellowcoding@gmail.com>
2024-05-24 17:51:49 -0700

    initial filestructure and example

---
+++ Problems/0001/Python/ComputationTime/solution.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complement = dict()
+        for i, num in enumerate(nums):
+            if num in complement:
+                return complement[num], i
+            else:
+                complement[target - num] = i
+        return None
