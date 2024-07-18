011c31f6c1273363a1e7aca9a73eb083dfd3d467
Batbileg Erdenebayar <batbilegerdenebayar@Batbilegs-MacBook-Pro.local>
2024-07-17 17:55:24 -0700

    leet code problem solved

---
+++ Problems/0001/Python/BatbilegErdene/Roman2Integer.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        roman_arabic = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        
+        integer = 0
+        s = s.replace("IV", "IIII")
+        s = s.replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX")
+        s = s.replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC")
+        s = s.replace("CM", "DCCCC")
+
+        for i in s:
+            integer += roman_arabic[i]
+        return integer
